1aa1f4cbb36f9f899f07cb9fd76fb3546df071eb
Gabriel-tehc <82235637+Gabriel-tehc@users.noreply.github.com>
2021-09-11 18:01:46 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+        /*
+    echo "Hola mundo";
+    echo "Amo PHP";
+    echo "A";
+    echo "B";
+    echo "C";
+    echo "<strong>Esto es un texto con negrita</strong>";
+        */
+        /*
+    $name = 'etb';
+    $age = 20;
+    echo $name;
+
+    define ("MSG", "Hola clientes", true);
+    echo MSG;
+
+    $string1 = "Hola mundo!";
+    $string2 = 'Hola mundo!';
+
+    $int1 = 42;
+    $int2 = -42
+
+    $x = 42.168;
+    $x = = true; $y = false;
+
+    $str = "10";
+    $int = 20;
+    $sum = $str + $int;
+    echo ($sum);
+        */
+        /*
+    $name = 'David';
+    function getName(){
+        echo $name;
+    }
+    getName();
+        */
+        /*
+    $name = 'John';
+    function getName(){
+        global $name;
+        echo $name;
+    }
+    getName();
+
+    $a = 'Hola';
+    $hello = "HOLA!";
+    echo $$a;
+        */
+        /*
+    $num1 = 8;
+    $num2 = 6;
+
+    //Suma
+    echo $num1 + $num2;
+    //Resta
+    echo $num1 - $num2;
+    //Multiplicación
+    echo $num1 * $num2;
+    //División
+    echo $num1 / $num2;
+        */
+        /*
+    $x = 14;
+    $y = 3;
+    echo $x % $y
+
+    $a = 2; $b = $a++;
+    $a = 2; $b = ++$a;
+
+    $names = array ("David", "John", "Rupert");
+        */
+        /*
+    $myarray[0] = "John";
+    $myarray[1] = "<strong>PHP</strong>";
+    $myarray[2] = "21";
+
+    echo "$myarray[0] is $myarray[2] and knows $myarray[1]"
+        */
+        /*
+    $people = array ( "david"=>"27", "John"=> "21", "Rupert"=> "42");
+
+    echo $people ['John'];
+        */
+        /*
+    $people = array(
+    'Online' => array('David', 'John'),
+    'offline' => array ('John', 'Rupert', 'Frank'),
+    'away' => array('Arthur', 'Daniel')
+    );
+        */
+        /*
+    $i = 1;
+    while ($i <7){
+        echo "El valor es $i <br/>";
+        $i++;
+    }
+        */
+        /*
+    $i = 5;
+    do {
+        echo "El Numero es" , $i , "<br/>";
+        $i++;
+    } while ($i <= 7);
+        */
+?>
